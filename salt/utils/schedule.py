45597a50664919ed0a5f7e365dc6af2a0fffa858
Bo Maryniuk <bo@suse.de>
2017-05-08 15:27:09 +0200

    Bugfix: deserialising crashes in the recursive loop (bsc#1036125)

--- salt/utils/schedule.py
+++ salt/utils/schedule.py
@@ -264,6 +264,7 @@ import threading
 import logging
 import errno
 import random
+import copy
 
 # Import Salt libs
 import salt.utils
@@ -678,7 +679,7 @@ class Schedule(object):
             if argspec.keywords:
                 # this function accepts **kwargs, pack in the publish data
                 for key, val in six.iteritems(ret):
-                    kwargs['__pub_{0}'.format(key)] = val
+                    kwargs['__pub_{0}'.format(key)] = copy.deepcopy(val)
 
             ret['return'] = self.functions[func](*args, **kwargs)
 
