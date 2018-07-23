2c4ea84af38aca525e516e0391ea831d1fe6f611
Bo Maryniuk <bo@suse.de>
2018-07-23 10:51:41 +0200

    Add support for Python 3.7
    Rename module to full wording
    
    Fix imports
    
    Fix docstring typo
    
    Fix CLI config
    
    Fix comments
    
    Fix docstrings
    
    Rename async function to asynchronous
    
    Change internal function signatures to avoid reserved word
    
    Remove internal variables/properties with the reserved words
    
    Fix local opts from CLI
    
    Fix log error/info/warning and exception messages
    
    Cleanup docstrings at module level
    
    Fix function signatures in Cassandra module
    
    Lintfix: PEP8 requires two empty lines
    
    Deprecate 'async' parameter in Mandrill API
    
    Revert api call: it is about "functionname_async" suffix.
    
    Add 'async' backward compatibility
    
    Update docstring
    
    Use kwargs instead of directly named parameters
    
    Support original API
    
    Fix nag-message
    
    Keep runner API unchanged
    
    fix unicode literals
    
    Remove async keyword, moving it into the kwargs.
    
    Fix configuration setting

--- salt/engines/slack.py
+++ salt/engines/slack.py
@@ -740,7 +740,7 @@ class SlackClient(object):
         :param interval: time to wait between ending a loop and beginning the next
 
         '''
-        log.debug('Going to run a command async')
+        log.debug('Going to run a command asynchronous')
         runner_functions = sorted(salt.runner.Runner(__opts__).functions)
         # Parse args and kwargs
         cmd = msg['cmdline'][0]
@@ -762,7 +762,7 @@ class SlackClient(object):
             log.debug('Command %s will run via runner_functions', cmd)
             # pylint is tripping
             # pylint: disable=missing-whitespace-after-comma
-            job_id_dict = runner.async(cmd, {'args': args, 'kwargs': kwargs})
+            job_id_dict = runner.asynchronous(cmd, {'args': args, 'kwargs': kwargs})
             job_id = job_id_dict['jid']
 
         # Default to trying to run as a client module.
