f7f431d4f11a3147bf2153970887f87768aae079
Bo Maryniuk <bo@suse.de>
2018-09-28 15:22:33 +0200

    Fix IPv6 scope (bsc#1108557)
    Fix ipaddress imports
    
    Remove unused import
    
    Fix ipaddress import
    
    Fix unicode imports in compat
    
    Override standard IPv6Address class
    
    Check version via object
    
    Isolate Py2 and Py3 mode
    
    Add logging
    
    Add debugging to the ip_address method (py2 and py3)
    
    Remove multiple returns and add check for address syntax
    
    Remove unnecessary variable for import detection
    
    Remove duplicated code
    
    Remove unnecessary operator
    
    Remove multiple returns
    
    Use ternary operator instead
    
    Remove duplicated code
    
    Move docstrings to their native places
    
    Add real exception message
    
    Add logging to the ip_interface
    
    Add scope on str
    
    Lintfix: mute not called constructors
    
    Add extra detection for hexadecimal packed bytes on Python2. This cannot be detected with type comparison, because bytes == str and at the same time bytes != str if compatibility is not around
    
    Fix py2 case where the same class cannot initialise itself on Python2 via super.
    
    Simplify checking clause
    
    Do not use introspection for method swap
    
    Fix wrong type swap
    
    Add Py3.4 old implementation's fix
    
    Lintfix
    
    Lintfix refactor: remove duplicate returns as not needed
    
    Revert method remapping with pylint updates
    
    Remove unnecessary manipulation with IPv6 scope outside of the IPv6Address object instance
    
    Lintfix: W0611
    
    Reverse skipping tests: if no ipaddress

--- salt/minion.py
+++ salt/minion.py
@@ -25,6 +25,7 @@ from binascii import crc32
 # Import Salt Libs
 # pylint: disable=import-error,no-name-in-module,redefined-builtin
 from salt.ext import six
+from salt._compat import ipaddress
 from salt.ext.six.moves import range
 from salt.utils.zeromq import zmq, ZMQDefaultLoop, install_zmq, ZMQ_VERSION_INFO
 import salt.transport.client
