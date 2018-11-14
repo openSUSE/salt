97748e688c11f01bb789f02665ccab541b6b999a
Bo Maryniuk <bo@suse.de>
2018-11-14 17:36:23 +0100

    Get os_arch also without RPM package installed
    backport pkg.rpm test
    
    Add pkg.rpm unit test case
    
    Fix docstring
    
    Add UT for getting OS architecture fallback, when no RPM found (initrd, e.g.)
    
    Add UT for OS architecture detection on fallback, when no CPU arch can be determined
    
    Add UT for OS arch detection when no CPU arch or machine can be determined
    
    Remove unsupported testcase

--- tests/unit/utils/test_pkg.py
+++ tests/unit/utils/test_pkg.py
@@ -2,51 +2,19 @@
 
 from __future__ import absolute_import, unicode_literals, print_function
 
-from tests.support.unit import TestCase
-from tests.support.mock import MagicMock, patch
+from tests.support.unit import TestCase, skipIf
+from tests.support.mock import Mock, MagicMock, patch, NO_MOCK, NO_MOCK_REASON
 import salt.utils.pkg
 from salt.utils.pkg import rpm
 
-
-class PkgUtilsTestCase(TestCase):
-    '''
-    TestCase for salt.utils.pkg module
-    '''
-    test_parameters = [
-        ("16.0.0.49153-0+f1", "", "16.0.0.49153-0+f1"),
-        ("> 15.0.0", ">", "15.0.0"),
-        ("< 15.0.0", "<", "15.0.0"),
-        ("<< 15.0.0", "<<", "15.0.0"),
-        (">> 15.0.0", ">>", "15.0.0"),
-        (">= 15.0.0", ">=", "15.0.0"),
-        ("<= 15.0.0", "<=", "15.0.0"),
-        ("!= 15.0.0", "!=", "15.0.0"),
-        ("<=> 15.0.0", "<=>", "15.0.0"),
-        ("<> 15.0.0", "<>", "15.0.0"),
-        ("= 15.0.0", "=", "15.0.0"),
-        (">15.0.0", ">", "15.0.0"),
-        ("<15.0.0", "<", "15.0.0"),
-        ("<<15.0.0", "<<", "15.0.0"),
-        (">>15.0.0", ">>", "15.0.0"),
-        (">=15.0.0", ">=", "15.0.0"),
-        ("<=15.0.0", "<=", "15.0.0"),
-        ("!=15.0.0", "!=", "15.0.0"),
-        ("<=>15.0.0", "<=>", "15.0.0"),
-        ("<>15.0.0", "<>", "15.0.0"),
-        ("=15.0.0", "=", "15.0.0"),
-        ("", "", "")
-    ]
-
-    def test_split_comparison(self):
-        '''
-        Tests salt.utils.pkg.split_comparison
-        '''
-        for test_parameter in self.test_parameters:
-            oper, verstr = salt.utils.pkg.split_comparison(test_parameter[0])
-            self.assertEqual(test_parameter[1], oper)
-            self.assertEqual(test_parameter[2], verstr)
+try:
+    import pytest
+except ImportError:
+    pytest = None
 
 
+@skipIf(NO_MOCK, NO_MOCK_REASON)
+@skipIf(pytest is None, 'PyTest is missing')
 class PkgRPMTestCase(TestCase):
     '''
     Test case for pkg.rpm utils
