0f351af53fc45db1962b23c5aa3a9a69dbe473cb
Mihai Dinca <mdinca@suse.de>
2018-11-16 17:05:29 +0100

    Async batch implementation
    Add find_job checks
    
    Check if should close on all events
    
    Make batch_delay a request parameter
    
    Allow multiple event handlers
    
    Use config value for gather_job_timeout when not in payload
    
    Add async batch unittests
    
    Allow metadata to pass
    
    Pass metadata only to batch jobs
    
    Add the metadata to the start/done events
    
    Pass only metadata not all **kwargs
    
    Add separate batch presence_ping timeout

--- salt/utils/event.py
+++ salt/utils/event.py
@@ -857,6 +857,10 @@ class SaltEvent(object):
                     # Minion fired a bad retcode, fire an event
                     self._fire_ret_load_specific_fun(load)
 
+    def remove_event_handler(self, event_handler):
+        if event_handler in self.subscriber.callbacks:
+            self.subscriber.callbacks.remove(event_handler)
+
     def set_event_handler(self, event_handler):
         '''
         Invoke the event_handler callback each time an event arrives.
@@ -865,8 +869,10 @@ class SaltEvent(object):
 
         if not self.cpub:
             self.connect_pub()
+
+        self.subscriber.callbacks.add(event_handler)
         # This will handle reconnects
-        return self.subscriber.read_async(event_handler)
+        return self.subscriber.read_async()
 
     # pylint: disable=W1701
     def __del__(self):
