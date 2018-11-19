0adb218f7ed20e4b41535ed13ef1ae1b186a6d6a
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-11-19 11:46:26 +0000

    Remove arch from name when pkg.list_pkgs is called with 'attr' (bsc#1114029)
    Add unit tests for pkg_resource.format_pkg_list
    
    Fix pylint issues
    
    Refactor: Return requested attr even if empty
    
    Add corner cases on package names to unit tests
    
    Fix Zypper/Yum unit test after returning empty requested attrs
    
    Add Yum/Zypper list_pkgs unit tests for multiple versions reported
    
    Compare testing items properly to avoid unwanted failures
    
    Use assertCountEqual when running on Python3
    
    Add missing import for the six module
    
    Strip architecture from package name in aptpkg module
    
    Use parse_arch_from_name if available on the virtual pkg module
    
    Adapt unit tests after introducing parse_arch_from_name
    
    Use PKG_ARCH_SEPARATOR in pkg.normalize_name method
    
    Add pkg_resource to setup loader modules. Fix pylint
    
    Remove unnecessary lambda
    
    Return None instead empty string for arch and release in pkg.list_pkgs

--- salt/modules/yumpkg.py
+++ salt/modules/yumpkg.py
@@ -442,7 +442,7 @@ def normalize_name(name):
     return name
 
 
-def parse_arch(name):
+def parse_arch_from_name(name):
     '''
     Parse name and architecture from the specified package name.
 
@@ -450,7 +450,7 @@ def parse_arch(name):
 
     .. code-block:: bash
 
-        salt '*' pkg.parse_arch zsh.x86_64
+        salt '*' pkg.parse_arch_from_name zsh.x86_64
     '''
     _name, _arch = None, None
     try:
