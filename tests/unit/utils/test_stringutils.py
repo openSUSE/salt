42036632c0fa04ab84d52399d6e3b99e5db87db3
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2018-12-04 15:31:11 +0000

    Fix latin1 encoding problems on file module (bsc#1116837)
    Add to_str option to decode funcs
    
    This allows for the string to be forced to a str type instead of
    unicode on PY2.
    
    ldapmod.py/ldap3.py: Force modlist for search/modify/etc. to be str types
    
    Add get_diff to salt.utils.stringutils
    
    Make to_unicode/to_str/to_bytes helpers attempt latin-1
    
    Also allow for multiple encodings to be passed
    
    Use new get_diff helper in file module
    
    Use BASE_FILES instead of redundant STATE_DIR
    
    Add integration test for latin-1 file diffs
    
    PY3 scoping fix
    
    In PY3 the caught exceptions now drop out of scope when leaving the for
    loop.
    
    Add unit test for latin-1 fallback, multi-encoding
    
    Skip pylint false-positives
    
    Fix incorrect use of __salt__ when __utils__ is needed
    
    Add stringutils.get_diff to mocks
    
    Only try latin-1 from get_diff instead of by default
    
    Fix to_unicode test
    
    Since latin-1 is not being automatically decoded, we need to explicitly
    pass it on the test.
    
    Fix use of with_tempfile decorator
    
    Add ability to run a shell command to depends decorator
    
    This adds the ability to suppress loading of a function based on the
    retcode of a command.
    
    Fix rst formatting
    
    Ignore all docstrings in runtests_decorators.py
    
    Remove unnecessary docstrings
    
    Update test to reflect docstring changes
    
    line function refactored to work on list
    
    Revert "line function refactored to work on list"
    
    This reverts commit a4969fc280ed4a04348b1f9bb3d594acd9a79286.
    
    _get_line_indent renamed to _set_line_indent
    
    _regex_to_static refactored to work on lists
    
    line function refactored to work on list
    
    Added _set_line_eol and _get_eol functions
    
    Setting end of line
    
    Make tests green
    
    test_line_insert_end fixed
    
    /sr.* pattern should raise exception
    
    file.line function refactored
    
    Make integration green. Added test for mode ensure insert before first line
    
    Fixed file permissions
    
    Removed regex compilation
    
    Comprehensions converting to unicode replaced by salt.utils.data.decode_list
    
    Empty match on delete or replace not causing IndexError exception
    
    List comprehension replaced
    
    Added comments

--- tests/unit/utils/test_stringutils.py
+++ tests/unit/utils/test_stringutils.py
@@ -18,6 +18,9 @@ STR = BYTES = UNICODE.encode('utf-8')
 # code points. Do not modify it.
 EGGS = '\u044f\u0438\u0306\u0446\u0430'
 
+LATIN1_UNICODE = 'räksmörgås'
+LATIN1_BYTES = LATIN1_UNICODE.encode('latin-1')
+
 
 class StringutilsTestCase(TestCase):
     def test_contains_whitespace(self):
@@ -111,6 +114,13 @@ class StringutilsTestCase(TestCase):
             'яйца'
         )
 
+        self.assertEqual(
+            salt.utils.stringutils.to_unicode(
+                LATIN1_BYTES, encoding='latin-1'
+            ),
+            LATIN1_UNICODE
+        )
+
         if six.PY3:
             self.assertEqual(salt.utils.stringutils.to_unicode('plugh'), 'plugh')
             self.assertEqual(salt.utils.stringutils.to_unicode('áéíóúý'), 'áéíóúý')
@@ -123,6 +133,10 @@ class StringutilsTestCase(TestCase):
             # Test utf-8 fallback with ascii default encoding
             with patch.object(builtins, '__salt_system_encoding__', 'ascii'):
                 self.assertEqual(salt.utils.stringutils.to_unicode(u'Ψ'.encode('utf-8')), u'Ψ')
+
+    def test_to_unicode_multi_encoding(self):
+        result = salt.utils.stringutils.to_unicode(LATIN1_BYTES, encoding=('utf-8', 'latin1'))
+        assert result == LATIN1_UNICODE
 
     def test_build_whitespace_split_regex(self):
         expected_regex = '(?m)^(?:[\\s]+)?Lorem(?:[\\s]+)?ipsum(?:[\\s]+)?dolor(?:[\\s]+)?sit(?:[\\s]+)?amet\\,' \
