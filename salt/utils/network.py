3d67cca67a27887e848f3e912acd700b6d2fa9cc
Bo Maryniuk <bo@suse.de>
2019-01-29 11:11:38 +0100

    Include aliases in the fqdns grains
    Add UT for "is_fqdn"
    
    Add "is_fqdn" check to the network utils
    
    Bugfix: include FQDNs aliases
    
    Deprecate UnitTest assertion in favour of built-in assert keyword
    
    Add UT for fqdns aliases
    
    Leverage cached interfaces, if any.

--- salt/utils/network.py
+++ salt/utils/network.py
@@ -1819,3 +1819,15 @@ def dns_check(addr, port, safe=False, ipv6=None):
             raise SaltClientError()
         raise SaltSystemExit(code=42, msg=err)
     return resolved
+
+
+def is_fqdn(hostname):
+    """
+    Verify if hostname conforms to be a FQDN.
+
+    :param hostname: text string with the name of the host
+    :return: bool, True if hostname is correct FQDN, False otherwise
+    """
+
+    compliant = re.compile(r"(?!-)[A-Z\d\-\_]{1,63}(?<!-)$", re.IGNORECASE)
+    return "." in hostname and len(hostname) < 0xff and all(compliant.match(x) for x in hostname.rstrip(".").split("."))
