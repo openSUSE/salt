382d71c02c5073cb79e4f3d80edb3977f4af4494
Alberto Planas <aplanas@gmail.com>
2019-07-30 11:23:12 +0200

    Accumulated changes required for Yomi (#165)
    * cmdmod: fix runas and group in run_chroot
    
    The parameters runas and group for cmdmod.run() will change the efective
    user and group before executing the command. But in a chroot environment is
    expected that the change happends inside the chroot, not outside, as the
    user and groups are refering to objects that can only exist inside the
    environment.
    
    This patch add the userspec parameter to the chroot command, to change
    the user in the correct place.
    
    (cherry picked from commit f0434aaeeee3ace4e3fc65c04e69984f08b2541e)
    
    * chroot: add missing sys directory
    
    (cherry picked from commit cdf74426bcad4e8bf329bf604c77ea83bfca8b2c)
    
    * chroot: change variable name to root
    
    (cherry picked from commit 7f68b65b1b0f9eec2a6b07b02714ead0121f0e4b)
    
    * chroot: fix bug in safe_kwargs iteration
    
    (cherry picked from commit 39da1c69ea2781bed6e9d8e6879b70d65fa5a5b0)
    
    * test_cmdmod: fix test_run_cwd_in_combination_with_runas
    
    (cherry picked from commit 42640ecf161caf64c61e9b02927882f92c850092)
    
    * test_cmdmod: add test_run_chroot_runas test
    
    (cherry picked from commit d900035089a22f6741d2095fd1f6694597041a88)
    
    * freezer: do not fail in cache dir is present
    
    (cherry picked from commit 25137c51e6d6e53e3099b6cddbf51d4cb2c53d8d)
    
    * freezer: clean freeze YAML profile on restore
    
    (cherry picked from commit 56b97c997257f12038399549dc987b7723ab225f)
    
    * zypperpkg: fix pkg.list_pkgs cache
    
    The cache from pkg.list_pkgs for the zypper installer is too aggresive.
    Some parameters will deliver different package lists, like root and
    includes. The current cache do not take those parameters into
    consideration, so the next time that this function is called, the last
    list of packages will be returned, without checking if the current
    parameters match the old one.
    
    This patch create a different cache key for each parameter combination,
    so the cached data will be separated too.
    
    (cherry picked from commit 9c54bb3e8c93ba21fc583bdefbcadbe53cbcd7b5)

--- salt/modules/chroot.py
+++ salt/modules/chroot.py
@@ -50,16 +50,17 @@ def __virtual__():
         return (False, 'Module chroot requires the command chroot')
 
 
-def exist(name):
+def exist(root):
     '''
     Return True if the chroot environment is present.
     '''
-    dev = os.path.join(name, 'dev')
-    proc = os.path.join(name, 'proc')
-    return all(os.path.isdir(i) for i in (name, dev, proc))
+    dev = os.path.join(root, 'dev')
+    proc = os.path.join(root, 'proc')
+    sys = os.path.join(root, 'sys')
+    return all(os.path.isdir(i) for i in (root, dev, proc, sys))
 
 
-def create(name):
+def create(root):
     '''
     Create a basic chroot environment.
 
@@ -67,7 +68,7 @@ def create(name):
     install the minimal required binaries, including Python if
     chroot.call is called.
 
-    name
+    root
         Path to the chroot environment
 
     CLI Example:
@@ -77,26 +78,28 @@ def create(name):
         salt myminion chroot.create /chroot
 
     '''
-    if not exist(name):
-        dev = os.path.join(name, 'dev')
-        proc = os.path.join(name, 'proc')
+    if not exist(root):
+        dev = os.path.join(root, 'dev')
+        proc = os.path.join(root, 'proc')
+        sys = os.path.join(root, 'sys')
         try:
             os.makedirs(dev, mode=0o755)
             os.makedirs(proc, mode=0o555)
+            os.makedirs(sys, mode=0o555)
         except OSError as e:
             log.error('Error when trying to create chroot directories: %s', e)
             return False
     return True
 
 
-def call(name, function, *args, **kwargs):
+def call(root, function, *args, **kwargs):
     '''
     Executes a Salt function inside a chroot environment.
 
     The chroot does not need to have Salt installed, but Python is
     required.
 
-    name
+    root
         Path to the chroot environment
 
     function
@@ -107,18 +110,19 @@ def call(name, function, *args, **kwargs):
     .. code-block:: bash
 
         salt myminion chroot.call /chroot test.ping
+        salt myminion chroot.call /chroot ssh.set_auth_key user key=mykey
 
     '''
 
     if not function:
         raise CommandExecutionError('Missing function parameter')
 
-    if not exist(name):
+    if not exist(root):
         raise CommandExecutionError('Chroot environment not found')
 
     # Create a temporary directory inside the chroot where we can
     # untar salt-thin
-    thin_dest_path = tempfile.mkdtemp(dir=name)
+    thin_dest_path = tempfile.mkdtemp(dir=root)
     thin_path = __utils__['thin.gen_thin'](
         __opts__['cachedir'],
         extra_mods=__salt__['config.option']('thin_extra_mods', ''),
@@ -130,7 +134,7 @@ def call(name, function, *args, **kwargs):
         return {'result': False, 'comment': stdout}
 
     chroot_path = os.path.join(os.path.sep,
-                               os.path.relpath(thin_dest_path, name))
+                               os.path.relpath(thin_dest_path, root))
     try:
         safe_kwargs = clean_kwargs(**kwargs)
         salt_argv = [
@@ -144,8 +148,8 @@ def call(name, function, *args, **kwargs):
             '-l', 'quiet',
             '--',
             function
-        ] + list(args) + ['{}={}'.format(k, v) for (k, v) in safe_kwargs]
-        ret = __salt__['cmd.run_chroot'](name, [str(x) for x in salt_argv])
+        ] + list(args) + ['{}={}'.format(k, v) for (k, v) in safe_kwargs.items()]
+        ret = __salt__['cmd.run_chroot'](root, [str(x) for x in salt_argv])
         if ret['retcode'] != EX_OK:
             raise CommandExecutionError(ret['stderr'])
 
