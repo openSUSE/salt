77d92030bd534e146ff0aed9c591d8d29ddd26ef
Jochen Breuer <jbreuer@suse.de>
2020-03-25 15:18:51 +0100

    Reintroducing reverted changes
    Reintroducing changes from commit e20362f6f053eaa4144583604e6aac3d62838419
    that got partially reverted by this commit:
    https://github.com/openSUSE/salt/commit/d0ef24d113bdaaa29f180031b5da384cffe08c64#diff-820e6ce667fe3afddbc1b9cf1682fdef

--- tests/unit/modules/test_aptpkg.py
+++ tests/unit/modules/test_aptpkg.py
@@ -253,7 +253,9 @@ class AptPkgTestCase(TestCase, LoaderModuleMockMixin):
             if installed['wget'].get(names[name], False):
                 installed['wget'][name] = installed['wget'].pop(names[name])
 
-        assert aptpkg.info_installed('wget') == installed
+        del installed['wget']['status']
+        self.assertEqual(aptpkg.info_installed('wget'), installed)
+        self.assertEqual(len(aptpkg.info_installed()), 1)
 
     @patch('salt.modules.aptpkg.__salt__', {'lowpkg.info': MagicMock(return_value=LOWPKG_INFO)})
     def test_info_installed_attr(self):
