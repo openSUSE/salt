21c04bbb6e6462ef89fb4a91c7c3f6c3ea26f480
Alberto Planas <aplanas@suse.com>
2020-04-08 15:45:37 +0200

    Fix test_core tests for fqdns errors

--- tests/unit/grains/test_core.py
+++ tests/unit/grains/test_core.py
@@ -1122,20 +1122,22 @@ class CoreGrainsTestCase(TestCase, LoaderModuleMockMixin):
 
         for errno in (0, core.HOST_NOT_FOUND, core.NO_DATA):
             mock_log = MagicMock()
-            with patch.object(socket, 'gethostbyaddr',
-                              side_effect=_gen_gethostbyaddr(errno)):
-                with patch('salt.grains.core.log', mock_log):
-                    self.assertEqual(core.fqdns(), {'fqdns': []})
-                    mock_log.debug.assert_called_once()
-                    mock_log.error.assert_not_called()
+            with patch.dict(core.__salt__, {'network.fqdns': salt.modules.network.fqdns}):
+                with patch.object(socket, 'gethostbyaddr',
+                                  side_effect=_gen_gethostbyaddr(errno)):
+                    with patch('salt.modules.network.log', mock_log):
+                        self.assertEqual(core.fqdns(), {'fqdns': []})
+                        mock_log.debug.assert_called()
+                        mock_log.error.assert_not_called()
 
         mock_log = MagicMock()
-        with patch.object(socket, 'gethostbyaddr',
-                          side_effect=_gen_gethostbyaddr(-1)):
-            with patch('salt.grains.core.log', mock_log):
-                self.assertEqual(core.fqdns(), {'fqdns': []})
-                mock_log.debug.assert_not_called()
-                mock_log.error.assert_called_once()
+        with patch.dict(core.__salt__, {'network.fqdns': salt.modules.network.fqdns}):
+            with patch.object(socket, 'gethostbyaddr',
+                              side_effect=_gen_gethostbyaddr(-1)):
+                with patch('salt.modules.network.log', mock_log):
+                    self.assertEqual(core.fqdns(), {'fqdns': []})
+                    mock_log.debug.assert_called_once()
+                    mock_log.error.assert_called_once()
 
     @patch.object(salt.utils.platform, 'is_windows', MagicMock(return_value=False))
     @patch('salt.utils.network.ip_addrs', MagicMock(return_value=['1.2.3.4', '5.6.7.8']))
