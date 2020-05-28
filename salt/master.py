fbf1ddb412a2d3feb4ae5fa1e7784a770ac6ea7d
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-05-28 09:37:08 +0100

    Add publish_batch to ClearFuncs exposed methods

--- salt/master.py
+++ salt/master.py
@@ -1947,6 +1947,7 @@ class ClearFuncs(TransportMethods):
     expose_methods = (
         "ping",
         "publish",
+        "publish_batch",
         "get_token",
         "mk_token",
         "wheel",
