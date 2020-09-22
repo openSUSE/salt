0821b05c7da7d0e06b61b421b9355d54286cb61d
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2020-09-22 15:15:51 +0100

    Invalidate file list cache when cache file modified time is in the future (bsc#1176397)
    Add test_future_file_list_cache_file_ignored unit test

--- tests/unit/test_fileserver.py
+++ tests/unit/test_fileserver.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 """
     :codeauthor: Joao Mesquita <jmesquita@sangoma.com>
 """
 
-# Import Python libs
-from __future__ import absolute_import, print_function, unicode_literals
 
+import datetime
+import os
+import time
+
+import salt.utils.files
 from salt import fileserver
-
-# Import Salt Testing libs
+from tests.support.helpers import with_tempdir
 from tests.support.mixins import LoaderModuleMockMixin
 from tests.support.unit import TestCase
 
@@ -31,22 +32,38 @@ class MapDiffTestCase(TestCase):
         assert fileserver.diff_mtime_map(map1, map2) is True
 
 
-class VCSBackendWhitelistCase(TestCase, LoaderModuleMockMixin):
+class VCSBackendWhitelistCase(TestCase):
     def setup_loader_modules(self):
         return {fileserver: {}}
 
-    def test_whitelist(self):
+    @with_tempdir()
+    def test_future_file_list_cache_file_ignored(self, cachedir):
         opts = {
-            "fileserver_backend": ["roots", "git", "hgfs", "svn"],
+            "fileserver_backend": ["roots"],
+            "cachedir": cachedir,
             "extension_modules": "",
         }
-        fs = fileserver.Fileserver(opts)
-        assert fs.servers.whitelist == [
-            "git",
-            "gitfs",
-            "hg",
-            "hgfs",
-            "svn",
-            "svnfs",
-            "roots",
-        ], fs.servers.whitelist
+
+        back_cachedir = os.path.join(cachedir, "file_lists/roots")
+        os.makedirs(os.path.join(back_cachedir))
+
+        # Touch a couple files
+        for filename in ("base.p", "foo.txt"):
+            with salt.utils.files.fopen(
+                os.path.join(back_cachedir, filename), "wb"
+            ) as _f:
+                if filename == "base.p":
+                    _f.write(b"\x80")
+
+        # Set modification time to file list cache file to 1 year in the future
+        now = datetime.datetime.utcnow()
+        future = now + datetime.timedelta(days=365)
+        mod_time = time.mktime(future.timetuple())
+        os.utime(os.path.join(back_cachedir, "base.p"), (mod_time, mod_time))
+
+        list_cache = os.path.join(back_cachedir, "base.p")
+        w_lock = os.path.join(back_cachedir, ".base.w")
+        ret = fileserver.check_file_list_cache(opts, "files", list_cache, w_lock)
+        assert (
+            ret[1] is True
+        ), "Cache file list cache file is not refreshed when future modification time"
