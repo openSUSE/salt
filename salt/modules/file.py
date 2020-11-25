08d185f957b86434f88ce36d1b78b9fec3a6ea44
Victor Zhestkov <vzhestkov@suse.com>
2020-11-25 15:09:41 +0300

    Fix salt.utils.stringutils.to_str calls to make it working with numeric uid/gid

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -4970,6 +4970,12 @@ def check_perms(
     is_dir = os.path.isdir(name)
     is_link = os.path.islink(name)
 
+    def __safe_to_str(s):
+        try:
+            return salt.utils.stringutils.to_str(s)
+        except:
+            return salt.utils.stringutils.to_str(str(s))
+
     # user/group changes if needed, then check if it worked
     if user:
         if isinstance(user, int):
@@ -4979,7 +4985,7 @@ def check_perms(
             and user_to_uid(user) != user_to_uid(perms["luser"])
         ) or (
             not salt.utils.platform.is_windows()
-            and salt.utils.stringutils.to_str(user) != perms["luser"]
+            and __safe_to_str(user) != perms["luser"]
         ):
             perms["cuser"] = user
 
@@ -4991,7 +4997,7 @@ def check_perms(
             and group_to_gid(group) != group_to_gid(perms["lgroup"])
         ) or (
             not salt.utils.platform.is_windows()
-            and salt.utils.stringutils.to_str(group) != perms["lgroup"]
+            and __safe_to_str(group) != perms["lgroup"]
         ):
             perms["cgroup"] = group
 
@@ -5023,8 +5029,7 @@ def check_perms(
             and user != ""
         ) or (
             not salt.utils.platform.is_windows()
-            and salt.utils.stringutils.to_str(user)
-            != get_user(name, follow_symlinks=follow_symlinks)
+            and __safe_to_str(user) != get_user(name, follow_symlinks=follow_symlinks)
             and user != ""
         ):
             if __opts__["test"] is True:
@@ -5045,8 +5050,7 @@ def check_perms(
             and group != ""
         ) or (
             not salt.utils.platform.is_windows()
-            and salt.utils.stringutils.to_str(group)
-            != get_group(name, follow_symlinks=follow_symlinks)
+            and __safe_to_str(group) != get_group(name, follow_symlinks=follow_symlinks)
             and group != ""
         ):
             if __opts__["test"] is True:
