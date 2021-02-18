40c6dd0d9d13bc75a3df4c7e2dd6564f9a425702
Victor Zhestkov <35733135+vzhestkov@users.noreply.github.com>
2021-02-18 14:49:38 +0300

    Add sleep on exception handling on minion connection attempt to the master (bsc#1174855) (#321)
    * Async batch implementation fix
    
    * Add sleep on exception handling on minion connection attempt to the master (bsc#1174855)

--- salt/minion.py
+++ salt/minion.py
@@ -1123,6 +1123,9 @@ class MinionManager(MinionBase):
         last = 0  # never have we signed in
         auth_wait = minion.opts["acceptance_wait_time"]
         failed = False
+        retry_wait = 1
+        retry_wait_inc = 1
+        max_retry_wait = 20
         while True:
             try:
                 if minion.opts.get("beacons_before_connect", False):
@@ -1158,6 +1161,9 @@ class MinionManager(MinionBase):
                     minion.opts["master"],
                     exc_info=True,
                 )
+                yield salt.ext.tornado.gen.sleep(retry_wait)
+                if retry_wait < max_retry_wait:
+                    retry_wait += retry_wait_inc
 
     # Multi Master Tune In
     def tune_in(self):
