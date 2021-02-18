41bb712f2bdcbddb016b5138d1c3a26c99c9e53c
Victor Zhestkov <35733135+vzhestkov@users.noreply.github.com>
2021-02-18 15:56:01 +0300

    Implementation of suse_ip execution module (bsc#1099976) (#323)

--- setup.py
+++ setup.py
@@ -1108,6 +1108,7 @@ class SaltDistribution(distutils.dist.Distribution):
         package_data = {
             "salt.templates": [
                 "rh_ip/*.jinja",
+                "suse_ip/*.jinja",
                 "debian_ip/*.jinja",
                 "virt/*.jinja",
                 "git/*",
