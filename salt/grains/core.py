72055225207be7f3b13e3297bea797346e04d8ad
Pau Garcia Quiles <pau.garcia@suse.com>
2021-04-04 02:41:16 +0200

    Backport of https://github.com/saltstack/salt/pull/59687

--- salt/grains/core.py
+++ salt/grains/core.py
@@ -1468,7 +1468,7 @@ _OS_NAME_MAP = {
     'slesexpand': 'RES',
     'linuxmint': 'Mint',
     'neon': 'KDE neon',
-    'alibaba': 'Alibaba Cloud (Aliyun)',
+    'alibabaclo': 'Alinux',
 }
 
 # Map the 'os' grain to the 'os_family' grain
@@ -1543,7 +1543,7 @@ _OS_FAMILY_MAP = {
     'AIX': 'AIX',
     'TurnKey': 'Debian',
     'AstraLinuxCE': 'Debian',
-    'Alibaba Cloud (Aliyun)': 'RedHat',
+    'Alinux': 'RedHat',
 }
 
 # Matches any possible format:
