aa0f845e2bbc37332db04c583f475cfe25304db6
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2021-04-20 11:01:26 +0100

    Improvements on "ansiblegate" module (#354)
    * Allow collecting Ansible Inventory from a minion
    
    * Prevent crashing if ansible-playbook doesn't return JSON
    
    * Add new 'ansible.discover_playbooks' method
    
    * Include custom inventory when discovering Ansible playbooks
    
    * Enhance 'ansible.discover_playbooks' to accept a list of locations
    
    * Remove unused constants from Ansible utils
    
    * Avoid string concatenation to calculate extra cmd args
    
    * Add unit test for ansible.targets
    
    * Improve Ansible roster targetting
    
    * Add tests for new ansiblegate module functions
    
    * Fix issue dealing with ungrouped targets on inventory
    
    * Enable ansible utils for ansible roster tests
    
    * Remove unnecessary code from Ansible utils
    
    * Fix pylint issue
    
    * Fix issue in documentation

--- tests/pytests/unit/modules/test_ansiblegate.py
+++ tests/pytests/unit/modules/test_ansiblegate.py
@@ -8,8 +8,11 @@ import pytest
 import salt.modules.ansiblegate as ansible
 import salt.utils.path
 import salt.utils.platform
+import salt.config
+import salt.loader
 from salt.exceptions import LoaderError
 from tests.support.mock import MagicMock, MockTimedProc, patch
+from tests.support.runtests import RUNTIME_VARS
 
 pytestmark = pytest.mark.skipif(
     salt.utils.platform.is_windows(), reason="Not supported on Windows"
@@ -18,7 +21,7 @@ pytestmark = pytest.mark.skipif(
 
 @pytest.fixture
 def configure_loader_modules():
-    return {ansible: {}}
+    return {ansible: {"__utils__": {}}}
 
 
 @pytest.fixture
@@ -201,3 +204,92 @@ def test_ansible_playbooks_return_retcode(resolver):
     ):
         ret = ansible.playbooks("fake-playbook.yml")
         assert "retcode" in ret
+
+
+def test_ansible_targets():
+    """
+    Test ansible.targets execution module function.
+    :return:
+    """
+    ansible_inventory_ret = """
+{
+    "_meta": {
+        "hostvars": {
+            "uyuni-stable-ansible-centos7-1.tf.local": {
+                "ansible_ssh_private_key_file": "/etc/ansible/my_ansible_private_key"
+            },
+            "uyuni-stable-ansible-centos7-2.tf.local": {
+                "ansible_ssh_private_key_file": "/etc/ansible/my_ansible_private_key"
+            }
+        }
+    },
+    "all": {
+        "children": [
+            "ungrouped"
+        ]
+    },
+    "ungrouped": {
+        "hosts": [
+            "uyuni-stable-ansible-centos7-1.tf.local",
+            "uyuni-stable-ansible-centos7-2.tf.local"
+        ]
+    }
+}
+    """
+    ansible_inventory_mock = MagicMock(return_value=ansible_inventory_ret)
+    with patch("salt.utils.path.which", MagicMock(return_value=True)):
+        opts = salt.config.DEFAULT_MINION_OPTS.copy()
+        utils = salt.loader.utils(opts, whitelist=["ansible"])
+        with patch("salt.modules.cmdmod.run", ansible_inventory_mock), patch.dict(
+            ansible.__utils__, utils), patch(
+            "os.path.isfile", MagicMock(return_value=True)
+        ):
+            ret = ansible.targets()
+            assert ansible_inventory_mock.call_args
+            assert "_meta" in ret
+            assert "uyuni-stable-ansible-centos7-1.tf.local" in ret["_meta"]["hostvars"]
+            assert "ansible_ssh_private_key_file" in ret["_meta"]["hostvars"]["uyuni-stable-ansible-centos7-1.tf.local"]
+            assert "all" in ret
+            assert len(ret["ungrouped"]["hosts"]) == 2
+
+
+def test_ansible_discover_playbooks_single_path():
+    playbooks_dir = os.path.join(
+        RUNTIME_VARS.TESTS_DIR, "unit/files/playbooks/example_playbooks/"
+    )
+    ret = ansible.discover_playbooks(playbooks_dir)
+    assert playbooks_dir in ret
+    assert ret[playbooks_dir]["playbook1.yml"] == {
+        "fullpath": os.path.join(playbooks_dir, "playbook1.yml")
+    }
+    assert ret[playbooks_dir]["example-playbook2/site.yml"] == {
+        "fullpath": os.path.join(playbooks_dir, "example-playbook2/site.yml"),
+        "custom_inventory": os.path.join(playbooks_dir, "example-playbook2/hosts"),
+    }
+
+
+def test_ansible_discover_playbooks_single_path_using_parameters():
+    playbooks_dir = os.path.join(
+        RUNTIME_VARS.TESTS_DIR, "unit/files/playbooks/example_playbooks/"
+    )
+    ret = ansible.discover_playbooks(
+        playbooks_dir, playbook_extension="foobar", hosts_filename="deadbeaf"
+    )
+    assert playbooks_dir in ret
+    assert ret[playbooks_dir] == {}
+
+
+def test_ansible_discover_playbooks_multiple_locations():
+    playbooks_dir = os.path.join(
+        RUNTIME_VARS.TESTS_DIR, "unit/files/playbooks/example_playbooks/"
+    )
+    ret = ansible.discover_playbooks(locations=[playbooks_dir, "/tmp/foobar"])
+    assert playbooks_dir in ret
+    assert "/tmp/foobar" in ret
+    assert ret[playbooks_dir]["playbook1.yml"] == {
+        "fullpath": os.path.join(playbooks_dir, "playbook1.yml")
+    }
+    assert ret[playbooks_dir]["example-playbook2/site.yml"] == {
+        "fullpath": os.path.join(playbooks_dir, "example-playbook2/site.yml"),
+        "custom_inventory": os.path.join(playbooks_dir, "example-playbook2/hosts"),
+    }
