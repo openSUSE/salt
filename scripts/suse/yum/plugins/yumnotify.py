d460629bf05f5b13885456d7e735fe806aa225f3
Victor Zhestkov <35733135+vzhestkov@users.noreply.github.com>
2021-11-09 16:19:56 +0300

    Fix the regression for yumnotify plugin (#456)

--- scripts/suse/yum/plugins/yumnotify.py
+++ scripts/suse/yum/plugins/yumnotify.py
@@ -63,4 +63,4 @@ def posttrans_hook(conduit):
                     )
                 )
         except OSError as e:
-            print("Unable to save the cookie file: %s" % (e), file=sys.stderr)
+            sys.stderr.write("Unable to save the cookie file: %s\n" % (e))
