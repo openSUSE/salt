797b256548cbcda0f3828c6d182c44a3815dd313
Alexander Graul <agraul@suse.com>
2022-01-18 17:10:37 +0100

    Include aliases in the fqdns grains
    Add UT for "is_fqdn"
    
    Add "is_fqdn" check to the network utils
    
    Bugfix: include FQDNs aliases
    
    Deprecate UnitTest assertion in favour of built-in assert keyword
    
    Add UT for fqdns aliases
    
    Leverage cached interfaces, if any.
    
    Implement network.fqdns module function (bsc#1134860) (#172)
    
    * Duplicate fqdns logic in module.network
    * Move _get_interfaces to utils.network
    * Reuse network.fqdns in grains.core.fqdns
    * Return empty list when fqdns grains is disabled
    
    Co-authored-by: Eric Siebigteroth <eric.siebigteroth@suse.de>

--- tests/unit/utils/test_network.py
+++ tests/unit/utils/test_network.py
@@ -1269,3 +1269,35 @@ class NetworkTestCase(TestCase):
             ),
         ):
             self.assertEqual(network.get_fqhostname(), host)
+
+    def test_is_fqdn(self):
+        """
+        Test is_fqdn function passes possible FQDN names.
+
+        :return: None
+        """
+        for fqdn in [
+            "host.domain.com",
+            "something.with.the.dots.still.ok",
+            "UPPERCASE.ALSO.SHOULD.WORK",
+            "MiXeD.CaSe.AcCePtAbLe",
+            "123.host.com",
+            "host123.com",
+            "some_underscore.com",
+            "host-here.com",
+        ]:
+            assert network.is_fqdn(fqdn)
+
+    def test_is_not_fqdn(self):
+        """
+        Test is_fqdn function rejects FQDN names.
+
+        :return: None
+        """
+        for fqdn in [
+            "hostname",
+            "/some/path",
+            "$variable.here",
+            "verylonghostname.{}".format("domain" * 45),
+        ]:
+            assert not network.is_fqdn(fqdn)
