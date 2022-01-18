6d7075c11c7d98f98b793738bca42307db2b7975
Alexander Graul <agraul@suse.com>
2022-01-18 16:36:57 +0100

    Use Adler32 algorithm to compute string checksums
    Generate the same numeric value across all Python versions and platforms
    
    Re-add getting hash by Python shell-out method
    
    Add an option to choose between default hashing, Adler32 or CRC32 algorithms
    
    Set default config option  for server_id hashing to False on minion
    
    Choose CRC method, default to faster but less reliable "adler32", if crc is in use
    
    Add warning for Sodium.
    
    Move server_id deprecation warning to reduce log spamming (bsc#1135567) (bsc#1135732)
    
    Remove deprecated warning that breaks miniion execution when "server_id_use_crc" opts are missing

--- salt/config/__init__.py
+++ salt/config/__init__.py
@@ -964,6 +964,9 @@ VALID_OPTS = immutabletypes.freeze(
         # The port to be used when checking if a master is connected to a
         # minion
         "remote_minions_port": int,
+        # Use Adler32 hashing algorithm for server_id (default False until Sodium, "adler32" after)
+        # Possible values are: False, adler32, crc32
+        "server_id_use_crc": (bool, str),
     }
 )
 
@@ -1267,6 +1270,7 @@ DEFAULT_MINION_OPTS = immutabletypes.freeze(
         "disabled_requisites": [],
         "reactor_niceness": None,
         "fips_mode": False,
+        "server_id_use_crc": False,
     }
 )
 
