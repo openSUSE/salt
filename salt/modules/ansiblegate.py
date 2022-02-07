11becede99005ef41020e7a110f917b4d8f68419
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-02-07 15:31:23 +0000

    Properly get separated copies from list

--- salt/modules/ansiblegate.py
+++ salt/modules/ansiblegate.py
@@ -45,14 +45,16 @@ hosts:
 """
 DEFAULT_TIMEOUT = 1200  # seconds (20 minutes)
 
-__load__ = __non_ansible_functions__ = [
+__non_ansible_functions__ = []
+
+__load__ = __non_ansible_functions__[:] = [
     "help",
     "list_",
     "call",
     "playbooks",
     "discover_playbooks",
     "targets",
-][:]
+]
 
 
 def _set_callables(modules):
