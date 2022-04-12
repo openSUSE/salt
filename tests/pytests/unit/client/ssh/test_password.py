14fe0cd92c97a19ef4332e09fb30a89aa770941f
Victor Zhestkov <vzhestkov@suse.com>
2022-04-12 10:06:43 +0300

    Prevent affection of SSH.opts with LazyLoader (bsc#1197637)
    * Prevent affection SSH.opts with LazyLoader
    
    * Restore parsed targets
    
    * Fix test_ssh unit tests
    
    Adjust unit tests

--- tests/pytests/unit/client/ssh/test_password.py
+++ tests/pytests/unit/client/ssh/test_password.py
@@ -27,6 +27,8 @@ def test_password_failure(temp_salt_master, tmp_path):
     opts["argv"] = ["test.ping"]
     opts["selected_target_option"] = "glob"
     opts["tgt"] = "localhost"
+    opts["ssh_cli_tgt"] = "localhost"
+    opts["_ssh_version"] = "foobar"
     opts["arg"] = []
     roster = str(tmp_path / "roster")
     handle_ssh_ret = [
@@ -44,7 +46,7 @@ def test_password_failure(temp_salt_master, tmp_path):
         "salt.client.ssh.SSH.handle_ssh", MagicMock(return_value=handle_ssh_ret)
     ), patch("salt.client.ssh.SSH.key_deploy", MagicMock(return_value=expected)), patch(
         "salt.output.display_output", display_output
-    ):
+    ), patch("salt.client.ssh.ssh_version", MagicMock(return_value="foobar")):
         client = ssh.SSH(opts)
         ret = next(client.run_iter())
         with pytest.raises(SystemExit):
