c50f140ab195ff44ee6adbd525306a1cf6ed7ba2
Witek Bedyk <witold.bedyk@suse.com>
2022-05-17 16:21:25 +0200

    Make sure SaltCacheLoader use correct fileclient
    Backported from https://github.com/saltstack/salt/pull/61895
    
    Signed-off-by: Witek Bedyk <witold.bedyk@suse.com>

--- salt/state.py
+++ salt/state.py
@@ -4013,6 +4013,9 @@ class BaseHighState:
             )
         else:
             try:
+                # Make sure SaltCacheLoader use correct fileclient
+                if context is None:
+                    context = {"fileclient": self.client}
                 state = compile_template(
                     fn_,
                     self.state.rend,
