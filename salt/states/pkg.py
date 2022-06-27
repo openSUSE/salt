f2dc43cf1db3fee41e328c68545ccac2576021ca
Victor Zhestkov <vzhestkov@suse.com>
2022-06-27 18:01:21 +0300

    Normalize package names once with pkg.installed/removed using yum (bsc#1195895)
    * Normalize the package names only once on install/remove
    
    * Add test for checking pkg.installed/removed with only normalisation
    
    * Fix split_arch conditions
    
    * Fix test_pkg

--- salt/states/pkg.py
+++ salt/states/pkg.py
@@ -1873,6 +1873,7 @@ def installed(
                 normalize=normalize,
                 update_holds=update_holds,
                 ignore_epoch=ignore_epoch,
+                split_arch=False,
                 **kwargs
             )
         except CommandExecutionError as exc:
@@ -2940,7 +2941,7 @@ def _uninstall(
         }
 
     changes = __salt__["pkg.{}".format(action)](
-        name, pkgs=pkgs, version=version, **kwargs
+        name, pkgs=pkgs, version=version, split_arch=False, **kwargs
     )
     new = __salt__["pkg.list_pkgs"](versions_as_list=True, **kwargs)
     failed = []
