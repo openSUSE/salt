a1fc5287d501a1ecdbd259e5bbdd4f7d5d06dd13
Alexander Graul <agraul@suse.com>
2023-04-28 09:41:28 +0200

    Make sure the file client is destroyed upon used
    Backport of https://github.com/saltstack/salt/pull/64113

--- salt/pillar/__init__.py
+++ salt/pillar/__init__.py
@@ -9,7 +9,6 @@ import logging
 import os
 import sys
 import traceback
-import uuid
 
 import salt.channel.client
 import salt.ext.tornado.gen
@@ -1351,6 +1350,11 @@ class Pillar:
         if hasattr(self, "_closing") and self._closing:
             return
         self._closing = True
+        if self.client:
+            try:
+                self.client.destroy()
+            except AttributeError:
+                pass
 
     # pylint: disable=W1701
     def __del__(self):
