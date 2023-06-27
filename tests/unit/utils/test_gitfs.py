e8d4e857100309f6324c0df1f6bc3c0328eb9683
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-06-27 11:24:39 +0100

    3006.0: Prevent _pygit2.GitError: error loading known_hosts when $HOME is not set (bsc#1210994) (#588)
    * Prevent _pygit2.GitError: error loading known_hosts when $HOME is not set
    
    * Add unit test to cover case of unset home

--- tests/unit/utils/test_gitfs.py
+++ tests/unit/utils/test_gitfs.py
@@ -13,6 +13,9 @@ import salt.utils.files
 import salt.utils.gitfs
 import salt.utils.path
 import salt.utils.platform
+import tests.support.paths
+from salt.exceptions import FileserverConfigError
+from tests.support.helpers import patched_environ
 from tests.support.mixins import AdaptedConfigurationTestCaseMixin
 from tests.support.unit import TestCase
 
@@ -171,3 +174,147 @@ class TestGitBase(TestCase, AdaptedConfigurationTestCaseMixin):
             self.assertRaises(TimeoutError, provider.clear_lock)
         finally:
             provider._master_lock.release()
+
+
+@pytest.mark.skipif(not HAS_PYGIT2, reason="This host lacks proper pygit2 support")
+@pytest.mark.skip_on_windows(
+    reason="Skip Pygit2 on windows, due to pygit2 access error on windows"
+)
+class TestPygit2(TestCase):
+    def _prepare_remote_repository(self, path):
+        shutil.rmtree(path, ignore_errors=True)
+
+        filecontent = "This is an empty README file"
+        filename = "README"
+
+        signature = pygit2.Signature(
+            "Dummy Commiter", "dummy@dummy.com", int(time()), 0
+        )
+
+        repository = pygit2.init_repository(path, False)
+        builder = repository.TreeBuilder()
+        tree = builder.write()
+        commit = repository.create_commit(
+            "HEAD", signature, signature, "Create master branch", tree, []
+        )
+        repository.create_reference("refs/tags/simple_tag", commit)
+
+        with salt.utils.files.fopen(
+            os.path.join(repository.workdir, filename), "w"
+        ) as file:
+            file.write(filecontent)
+
+        blob = repository.create_blob_fromworkdir(filename)
+        builder = repository.TreeBuilder()
+        builder.insert(filename, blob, pygit2.GIT_FILEMODE_BLOB)
+        tree = builder.write()
+
+        repository.index.read()
+        repository.index.add(filename)
+        repository.index.write()
+
+        commit = repository.create_commit(
+            "HEAD",
+            signature,
+            signature,
+            "Added a README",
+            tree,
+            [repository.head.target],
+        )
+        repository.create_tag(
+            "annotated_tag", commit, pygit2.GIT_OBJ_COMMIT, signature, "some message"
+        )
+
+    def _prepare_cache_repository(self, remote, cache):
+        opts = {
+            "cachedir": cache,
+            "__role": "minion",
+            "gitfs_disable_saltenv_mapping": False,
+            "gitfs_base": "master",
+            "gitfs_insecure_auth": False,
+            "gitfs_mountpoint": "",
+            "gitfs_passphrase": "",
+            "gitfs_password": "",
+            "gitfs_privkey": "",
+            "gitfs_provider": "pygit2",
+            "gitfs_pubkey": "",
+            "gitfs_ref_types": ["branch", "tag", "sha"],
+            "gitfs_refspecs": [
+                "+refs/heads/*:refs/remotes/origin/*",
+                "+refs/tags/*:refs/tags/*",
+            ],
+            "gitfs_root": "",
+            "gitfs_saltenv_blacklist": [],
+            "gitfs_saltenv_whitelist": [],
+            "gitfs_ssl_verify": True,
+            "gitfs_update_interval": 3,
+            "gitfs_user": "",
+            "verified_gitfs_provider": "pygit2",
+        }
+        per_remote_defaults = {
+            "base": "master",
+            "disable_saltenv_mapping": False,
+            "insecure_auth": False,
+            "ref_types": ["branch", "tag", "sha"],
+            "passphrase": "",
+            "mountpoint": "",
+            "password": "",
+            "privkey": "",
+            "pubkey": "",
+            "refspecs": [
+                "+refs/heads/*:refs/remotes/origin/*",
+                "+refs/tags/*:refs/tags/*",
+            ],
+            "root": "",
+            "saltenv_blacklist": [],
+            "saltenv_whitelist": [],
+            "ssl_verify": True,
+            "update_interval": 60,
+            "user": "",
+        }
+        per_remote_only = ("all_saltenvs", "name", "saltenv")
+        override_params = tuple(per_remote_defaults.keys())
+        cache_root = os.path.join(cache, "gitfs")
+        role = "gitfs"
+        shutil.rmtree(cache_root, ignore_errors=True)
+        provider = salt.utils.gitfs.Pygit2(
+            opts,
+            remote,
+            per_remote_defaults,
+            per_remote_only,
+            override_params,
+            cache_root,
+            role,
+        )
+        return provider
+
+    def test_checkout(self):
+        remote = os.path.join(tests.support.paths.TMP, "pygit2-repo")
+        cache = os.path.join(tests.support.paths.TMP, "pygit2-repo-cache")
+        self._prepare_remote_repository(remote)
+        provider = self._prepare_cache_repository(remote, cache)
+        provider.remotecallbacks = None
+        provider.credentials = None
+        provider.init_remote()
+        provider.fetch()
+        provider.branch = "master"
+        self.assertIn(provider.cachedir, provider.checkout())
+        provider.branch = "simple_tag"
+        self.assertIn(provider.cachedir, provider.checkout())
+        provider.branch = "annotated_tag"
+        self.assertIn(provider.cachedir, provider.checkout())
+        provider.branch = "does_not_exist"
+        self.assertIsNone(provider.checkout())
+
+    def test_checkout_with_home_env_unset(self):
+        remote = os.path.join(tests.support.paths.TMP, "pygit2-repo")
+        cache = os.path.join(tests.support.paths.TMP, "pygit2-repo-cache")
+        self._prepare_remote_repository(remote)
+        provider = self._prepare_cache_repository(remote, cache)
+        provider.remotecallbacks = None
+        provider.credentials = None
+        with patched_environ(__cleanup__=["HOME"]):
+            self.assertTrue("HOME" not in os.environ)
+            provider.init_remote()
+            provider.fetch()
+            self.assertTrue("HOME" in os.environ)
