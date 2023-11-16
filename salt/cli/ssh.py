6b44ea7d06ba6588edbeaef895b719370a14c76b
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2023-11-16 09:23:58 +0000

    Revert "Make sure configured user is properly set by Salt (bsc#1210994) (#596)" (#614)
    This reverts commit 5ea4add5c8e2bed50b9825edfff7565e5f6124f3.

--- salt/cli/ssh.py
+++ salt/cli/ssh.py
@@ -1,9 +1,7 @@
 import sys
 
 import salt.client.ssh
-import salt.defaults.exitcodes
 import salt.utils.parsers
-from salt.utils.verify import check_user
 
 
 class SaltSSH(salt.utils.parsers.SaltSSHOptionParser):
@@ -16,12 +14,6 @@ class SaltSSH(salt.utils.parsers.SaltSSHOptionParser):
             sys.argv += ["x", "x"]  # Hack: pass a mandatory two options
             # that won't be used anyways with -H or --hosts
         self.parse_args()
-
-        if not check_user(self.config["user"]):
-            self.exit(
-                salt.defaults.exitcodes.EX_NOUSER,
-                "Cannot switch to configured user for Salt. Exiting",
-            )
 
         ssh = salt.client.ssh.SSH(self.config)
         try:
