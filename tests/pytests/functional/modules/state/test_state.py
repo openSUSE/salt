fc8dbeb4b44d160a4dbb70d842f849306d508024
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2024-06-11 16:48:35 +0100

    Add extra flaky test

--- tests/pytests/functional/modules/state/test_state.py
+++ tests/pytests/functional/modules/state/test_state.py
@@ -754,6 +754,7 @@ def test_retry_option_success_parallel(state, state_tree, tmp_path):
 
 
 @pytest.mark.slow_test
+@pytest.mark.xfail(reason="This test is flaky")
 def test_retry_option_eventual_success(state, state_tree, tmp_path):
     """
     test a state with the retry option that should return True, eventually
