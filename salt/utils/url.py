85c86f086287009d2e5c959674d30aac69af9b28
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2025-01-21 10:57:45 +0000

    Fix issues that break Salt in Python 3.12 and 3.13

--- salt/utils/url.py
+++ salt/utils/url.py
@@ -5,7 +5,7 @@ URL utils
 
 import re
 import sys
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, urlunsplit
 
 import salt.utils.data
 import salt.utils.path
@@ -47,8 +47,7 @@ def create(path, saltenv=None):
     path = salt.utils.data.decode(path)
 
     query = "saltenv={}".format(saltenv) if saltenv else ""
-    url = salt.utils.data.decode(urlunparse(("file", "", path, "", query, "")))
-    return "salt://{}".format(url[len("file:///") :])
+    return f'salt://{salt.utils.data.decode(urlunsplit(("", "", path, query, "")))}'
 
 
 def is_escaped(url):
