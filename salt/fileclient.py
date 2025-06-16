127f0c262d6d56c9723f24b3585855ea390cd1f7
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-06-16 14:37:42 -0700

    Remove token to prevent decoding errors (#68084)
    Clean up verify_load calls in master request server
    
    Remove tok in salt.channel.ReqServer.validate_token so it is not passed
    to the request handlers.
    
    Add tests around payload token removal
    
    Add changelog for #68076

--- salt/fileclient.py
+++ salt/fileclient.py
@@ -1439,8 +1439,6 @@ class RemoteClient(Client):
         Return the metadata derived from the master_tops system
         """
         load = {"cmd": "_master_tops", "id": self.opts["id"], "opts": self.opts}
-        if self.auth:
-            load["tok"] = self.auth.gen_token(b"salt")
         return self.channel.send(load)
 
     def __enter__(self):
