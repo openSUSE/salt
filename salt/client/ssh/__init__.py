9884e998d6d6dff0c2ebedcd6bfd6e57a9b78207
vzhestkov <vzhestkov@suse.com>
2025-09-30 12:02:14 +0200

    Add python3.11 as preferable for salt-ssh to avoid tests fails

--- salt/client/ssh/__init__.py
+++ salt/client/ssh/__init__.py
@@ -157,7 +157,7 @@ SSH_PY_CODE='import base64;
 if [ -n "$DEBUG" ]
     then set -x
 fi
-PYTHON_CMDS="/var/tmp/venv-salt-minion/bin/python python3 /usr/libexec/platform-python python27 python2.7 python26 python2.6 python2 python"
+PYTHON_CMDS="/var/tmp/venv-salt-minion/bin/python python3.11 python3 /usr/libexec/platform-python python27 python2.7 python26 python2.6 python2 python"
 for py_cmd in $PYTHON_CMDS
 do
     if command -v "$py_cmd" >/dev/null 2>&1 && "$py_cmd" -c "import sys; sys.exit(not (sys.version_info >= (2, 6)));"
