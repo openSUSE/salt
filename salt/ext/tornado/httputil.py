0ef57a3820d395ea6045c730db2e50e81f762382
Alexander Graul <agraul@suse.com>
2025-11-24 17:42:32 +0100

    Backport 3006.17 security fixes (#739)
    * Add minimum_auth_version to enforce all security
    
    CVE-2025-62349 A misbehaving minion could downgrade it's auth protocol
    version.
    
    (cherry picked from commit 1c76dfbc127c4b02f874ac852425f4288dfa7b73)
    
    * Use more secure default setting
    
    (cherry picked from commit 792f4985cce74541511b67a0b905bfe2a760184d)
    
    * Update compat test master config
    
    (cherry picked from commit 670c8ac4fd5da19f834e4b8877291dc11143b123)
    
    * Patch tornado for BDSA-2024-3438
    
    (cherry picked from commit e49357a6b0e49970bb455e8c268b24e7e1047548)
    
    * Patch tornado for BDSA-2024-3439
    
    (cherry picked from commit 644ba4474750031d64e770bb47b9dca144d1af24)
    
    * Patch tornado for BDSA-2024-9026
    
    (cherry picked from commit 867a627b702e1b4f9e07298b7ffce01edbbc414c)
    
    * Junos module yaml loader fix (CVE-2025-62348)
    
    Fixed unsafe YAML loader usage in junos execution module
    
    (cherry picked from commit c17fd645edef208233dcac855615fced69409a00)
    
    ---------
    
    Co-authored-by: Daniel A. Wozniak <daniel.wozniak@broadcom.com>

--- salt/ext/tornado/httputil.py
+++ salt/ext/tornado/httputil.py
@@ -68,6 +68,9 @@ except ImportError:
     pass
 
 
+# To be used with str.strip() and related methods.
+HTTP_WHITESPACE = " \t"
+
 # RFC 7230 section 3.5: a recipient MAY recognize a single LF as a line
 # terminator and ignore any preceding CR.
 _CRLF_RE = re.compile(r"\r?\n")
@@ -188,12 +191,12 @@ class HTTPHeaders(MutableMapping):
         """
         if line[0].isspace():
             # continuation of a multi-line header
-            new_part = " " + line.lstrip()
+            new_part = " " + line.lstrip(HTTP_WHITESPACE)
             self._as_list[self._last_key][-1] += new_part
             self._dict[self._last_key] += new_part
         else:
             name, value = line.split(":", 1)
-            self.add(name, value.strip())
+            self.add(name, value.strip(HTTP_WHITESPACE))
 
     @classmethod
     def parse(cls, headers):
@@ -972,15 +975,20 @@ def split_host_and_port(netloc):
     return (host, port)
 
 
-_OctalPatt = re.compile(r"\\[0-3][0-7][0-7]")
-_QuotePatt = re.compile(r"[\\].")
-_nulljoin = "".join
+_unquote_sub = re.compile(r"\\(?:([0-3][0-7][0-7])|(.))").sub
+
+
+def _unquote_replace(m):
+    if m[1]:
+        return chr(int(m[1], 8))
+    else:
+        return m[2]
 
 
 def _unquote_cookie(str):
     """Handle double quotes and escaping in cookie values.
 
-    This method is copied verbatim from the Python 3.5 standard
+    This method is copied verbatim from the Python 3.13 standard
     library (http.cookies._unquote) so we don't have to depend on
     non-public interfaces.
     """
@@ -1001,30 +1009,7 @@ def _unquote_cookie(str):
     #    \012 --> \n
     #    \"   --> "
     #
-    i = 0
-    n = len(str)
-    res = []
-    while 0 <= i < n:
-        o_match = _OctalPatt.search(str, i)
-        q_match = _QuotePatt.search(str, i)
-        if not o_match and not q_match:  # Neither matched
-            res.append(str[i:])
-            break
-        # else:
-        j = k = -1
-        if o_match:
-            j = o_match.start(0)
-        if q_match:
-            k = q_match.start(0)
-        if q_match and (not o_match or k < j):  # QuotePatt matched
-            res.append(str[i:k])
-            res.append(str[k + 1])
-            i = k + 2
-        else:  # OctalPatt matched
-            res.append(str[i:j])
-            res.append(chr(int(str[j + 1 : j + 4], 8)))
-            i = j + 4
-    return _nulljoin(res)
+    return _unquote_sub(_unquote_replace, s)
 
 
 def parse_cookie(cookie):
