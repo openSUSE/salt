d4ccf20b9d57c12a2034442b837f0549adec0710
vzhestkov <vzhestkov@suse.com>
2025-12-22 11:18:20 +0100

    Apply the suggestion for string_to_bool to make it cleaner

--- salt/modules/aptpkg.py
+++ salt/modules/aptpkg.py
@@ -50,7 +50,7 @@ from salt.utils.pkg.deb import (
     Deb822Section,
     SourceEntry,
     SourcesList,
-    string_to_bool_int,
+    string_to_bool,
     _invalid,
 )
 
@@ -2650,7 +2650,7 @@ def mod_repo(repo, saltenv="base", aptkey=True, **kwargs):
 
     if not mod_source:
         if not aptkey and not (
-            kwargs["signedby"] or string_to_bool_int(kwargs.get("trusted", "no")) == 1
+            kwargs["signedby"] or string_to_bool(kwargs.get("trusted", "no"))
         ):
             raise SaltInvocationError(
                 "missing 'signedby' or 'trusted' option when apt-key is missing"
