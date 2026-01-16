f859336115190e3b41569fa3c52cd6f804b3afad
Alexander Graul <agraul@suse.com>
2026-01-16 11:07:48 +0100

    Backport "Add MAINTAIN (m) privilege to postgres module lookup tables"
    * Initial port of test/unit/module/test_postgres.py to pytests
    
    (cherry picked from commit a919a3ea8782b252a3e65ad8787f73a906cf7fa5)
    
    * Added tablespace_create test
    
    (cherry picked from commit 452f6bdf855ae4484145f48d3a433dc949e2a775)
    
    * Updated test per reviewer suggestion
    
    (cherry picked from commit d2481582f4879ab0ddc52a9c0415b3e5dbf55326)
    
    * Further update to test per reviewer comment
    
    (cherry picked from commit 4dbd8caec2bfd2bbaf4526320ad9583b8e17e6bc)
    
    * Migrated test_deb_postgre.py to pytests
    
    (cherry picked from commit 0963209cc3e20ba6dfa3abdd5d716f400dafa910)
    
    * Updates tests for tablespace_list, tablespace_exists and tablespace_remove
    
    (cherry picked from commit 7714b22f075b81150deb87cd7c2bed9b49f55d74)
    
    * Updated tests with tablespace alter tests
    
    (cherry picked from commit 3c4aa715ba8bb8f8533e3e1ff081e57db746d1f4)
    
    * Updated asserts and limit testing to Linux only
    
    (cherry picked from commit e919e8f63e0b2e72a531cfce0b3bbc052f14ca69)
    
    * Converted global constants to pytest fixtures per reviewer comments
    
    (cherry picked from commit c6e8fa3b1781c997bc9e1c83e28d3950c7507ad5)
    
    * add 'm = MAINTAIN' privilege to the maps in the postgres module
    
    (cherry picked from commit a6b8fe4e8ebe8a15a68852eeda64a045f6ff5e46)
    
    * add changelog entry for bugfix to #66962
    
    (cherry picked from commit d422032f21fde7cec8c4ac3f9d628dc24c1cc3da)
    
    * add reference to MAINTAIN to the docs
    
    (cherry picked from commit 0a77c112a15e5d98ef50e6492e2cac9adbde0449)
    
    * add MAINTAIN privilege to PostgreSQL tests
    
    (cherry picked from commit cb26d8d0eef62e08a7581f2a03b1a4bb17176c04)
    
    ---------
    
    Co-authored-by: David Murphy < dmurphy@saltstack.com>
    Co-authored-by: Jonas Maurus <jonas-github@maurus.net>

--- salt/modules/postgres.py
+++ salt/modules/postgres.py
@@ -100,6 +100,7 @@ _PRIVILEGES_MAP = {
     "X": "EXECUTE",
     "x": "REFERENCES",
     "d": "DELETE",
+    "m": "MAINTAIN",
     "*": "GRANT",
 }
 _PRIVILEGES_OBJECTS = frozenset(
@@ -115,7 +116,7 @@ _PRIVILEGES_OBJECTS = frozenset(
     )
 )
 _PRIVILEGE_TYPE_MAP = {
-    "table": "arwdDxt",
+    "table": "arwdDxtm",
     "tablespace": "C",
     "language": "U",
     "sequence": "rwU",
