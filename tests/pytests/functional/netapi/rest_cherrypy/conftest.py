168325a479c2d65e348f31dec01d99b1af6d43c9
vzhestkov <vzhestkov@suse.com>
2026-06-10 11:33:03 +0200

    Fix the failing tests

--- tests/pytests/functional/netapi/rest_cherrypy/conftest.py
+++ tests/pytests/functional/netapi/rest_cherrypy/conftest.py
@@ -29,7 +29,7 @@ def app(client_config, load_auth):
 
 
 @pytest.fixture
-def http_server(io_loop, app, netapi_port):
+def http_server(app, netapi_port, io_loop):
     with netapi.TestsTornadoHttpServer(
         io_loop=io_loop, app=app, port=netapi_port
     ) as server:
